dba3f528b15dd6f64f7a6c47839cbb17d68f592b
Puneeth <hs.puneeth@outlook.com>
2025-02-12 00:35:04 +0530

    add custom widgets
    add custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 103, 58, 183),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 29.0,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );

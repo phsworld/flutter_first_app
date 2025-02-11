f7ecbc7f4234edc5d334cad8bc9e6ed8db6bc234
Puneeth <hs.puneeth@outlook.com>
2025-02-11 23:57:57 +0530

    Simple Program
    Simple Program

---
+++ .dart_tool/flutter_build/6e3676293df78b7a7be6abecbaab6c51/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\src\\flutter\\flutter_first_app\\.dart_tool\\flutter_build\\6e3676293df78b7a7be6abecbaab6c51\\native_assets.yaml: 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 103, 58, 183),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 29.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
